ead8c89714539d3eba3d411ba2237aa2de8cbc25
Auto Commit <auto@local>
2026-01-11 13:43:11 +0530

    udpate the code

--- testing.py
+++ testing.py
@@ -5,5 +5,21 @@ app = FastAPI()
 
 user_db = {
     1:{"name":"Dharmik", "age":21},
-    2:{"name":"John", "age":25},
+    2:{"name":"Skano", "age":25},
+    3:{"name":"John", "age":30}
 }
+
+class User(BaseModel):
+    name: str
+    age: int
+
+@app.put("/user/update/{id}")
+def user_update(id: int, user:User):
+    if id in user_db:
+        user_db[id] = user.dict()
+        print(user_db)  
+        return {"message": "User updated successfully", "user": user_db[id]}
+    else:
+        return {"error": "User not found"}
+
+
